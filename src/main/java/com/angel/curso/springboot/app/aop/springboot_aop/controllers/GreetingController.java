e4c8afe03bf6f20ae1afec90a6692c9915d04c85
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-06-04 18:17:49 +0200

    Añadiendo componente Service

--- src/main/java/com/angel/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
+++ src/main/java/com/angel/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
@@ -1,5 +1,24 @@
 package com.angel.curso.springboot.app.aop.springboot_aop.controllers;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.angel.curso.springboot.app.aop.springboot_aop.services.GreetingService;
+
+@RestController
 public class GreetingController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<?> greeting() {
+        return ResponseEntity
+                .ok(Collections.singletonMap("greeting", greetingService.sayHello("Pepe", "Hola que tal!")));
+    }
 
 }
