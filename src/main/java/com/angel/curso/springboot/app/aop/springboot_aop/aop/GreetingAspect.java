72b054074b458a5d34cf1cac777eb44044449257
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-06-10 20:54:22 +0200

    Advice Around

--- src/main/java/com/angel/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/angel/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,13 +3,15 @@ package com.angel.curso.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory; 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -33,6 +35,7 @@ public class GreetingAspect {
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues: " + method + " con los argumentos " + args);
     }
+
     @AfterReturning("execution(* com.angel.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
     public void loggerAfterReturning(JoinPoint joinPoint) {
 
@@ -40,11 +43,29 @@ public class GreetingAspect {
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues de retornar: " + method + " con los argumentos " + args);
     }
+
     @AfterThrowing("execution(* com.angel.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
     public void loggerAfterThrowing(JoinPoint joinPoint) {
 
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues de lanzar la excepcion: " + method + " con los argumentos " + args);
+    }
+
+    @Around("execution(* com.angel.curso.springboot.app.aop.springboot_aop.services.*.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        Object result;
+
+        try {
+            logger.info("El metodo " + method + "() con los parametros " + args);
+            result = joinPoint.proceed();
+            logger.info("El metodo " + method + "() retorna el result " + result);
+            return result;
+        } catch (Throwable e) {
+            logger.error("Error en la llamada del metodo" + method + "()");
+            throw e;
+        }
     }
 }
